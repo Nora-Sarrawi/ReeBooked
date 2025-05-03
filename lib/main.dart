9e97c58cff7290f28e5e2654e500b328c9109e8b
Kareem-004 <kareem.abukharma19@gmail.com>
2025-05-04 00:51:13 +0300

    edited main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import './views/my_books/my_books_screen.dart';
-import './views/requests/requests_screen.dart';
+import 'app.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: RequestDetailsScreen(),
-    ),
-  );
+  runApp(const ReBooked());
 }
