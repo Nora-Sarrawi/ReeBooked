0ea43e2761735c4023bb41244563a760573dd962
Your Name <you@example.com>
2025-05-04 00:16:06 +0300

    did custom text field , book details screen , swap requests screen and validation tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
-
+import './views/books/book_detail_screen.dart';
 void main() {
-  runApp(const ReBooked());
+  runApp( BookDetailsScreen());
 }
